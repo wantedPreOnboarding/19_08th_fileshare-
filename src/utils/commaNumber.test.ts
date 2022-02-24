0de7131b6050e99a4a2c68a6f2926d3c18d3cf81
DABIN JEONG <41@b41.kr>
2022-02-24 18:36:07 +0900

    Add - 3자리 콤마 유틸
    - 이전에 구현한 코드 추가

---
+++ src/utils/commaNumber.test.ts
@@ -0,0 +1,13 @@
+import commaNumber from "./commaNumber";
+
+describe("commaNumber", () => {
+  test('commaNumber(100) => "100"', () => {
+    expect(commaNumber(100)).toBe("100");
+  });
+  test('commaNumber(1000) => "1,000"', () => {
+    expect(commaNumber(1000)).toBe("1,000");
+  });
+  test('commaNumber(9999999) => "9,999,999"', () => {
+    expect(commaNumber(9999999)).toBe("9,999,999");
+  });
+});
