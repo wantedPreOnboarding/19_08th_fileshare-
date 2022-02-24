4ca7c46564de1853deed8b12ee063a341eed5f49
DABIN JEONG <41@b41.kr>
2022-02-24 18:32:20 +0900

    Add - 파일 사이즈 단위 계산 유틸 추가
    - 숫자 값과 소수점 단위를 입력하면 객체 형태로 '값'과 '크기 타입'을 출력

---
+++ src/utils/formatFileSize.ts
@@ -0,0 +1,16 @@
+export function formatFileSize(
+  bytes: number,
+  decimals = 2
+): { unit: string; volumNum: number } {
+  if (bytes === 0) {
+    return { volumNum: 0, unit: "Bytes" };
+  }
+  const k = 1000;
+  const dm = decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return {
+    volumNum: parseFloat((bytes / Math.pow(k, i)).toFixed(dm)),
+    unit: sizes[i],
+  };
+}
