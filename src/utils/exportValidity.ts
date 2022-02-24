68cfde6841b20555df020bf57dd9a1281ae7685d
DABIN JEONG <41@b41.kr>
2022-02-24 23:06:59 +0900

    Add - 유효기간 계산, 출력 유틸 추가
    - 타임스템프로 유효기간 값을 입력하면 남은 시간을 표시
    - 48시간 이상인 경우 남은 날짜로 표시
    - 48시간 미만인 경우 시간과 분으로 표시
    - 시간이 지난 경우 '만료'로 표시

---
+++ src/utils/exportValidity.ts
@@ -0,0 +1,16 @@
+export default function exportValidity(expires_at: number) {
+  const expire = new Date(expires_at * 1000);
+  const current = new Date();
+  const gap = expire.getTime() - current.getTime();
+  const gapDays = Math.floor(gap / (1000 * 60 * 60 * 24));
+  const gapHours = Math.floor(gap / (1000 * 60 * 60));
+  const gapMinutes = Math.floor(gap / (1000 * 60)) - gapHours * 60;
+
+  if (gap < 0) {
+    return "만료됨";
+  } else if (gapHours < 48) {
+    return `${gapHours}시간 ${gapMinutes}분`;
+  } else {
+    return `${gapDays}일`;
+  }
+}
