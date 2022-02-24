4bcc42c16fe7b9367e230a6364f91c9c7cbe5dc5
DABIN JEONG <41@b41.kr>
2022-02-24 18:33:02 +0900

    Add - 파일 사이즈 단위 유틸 테스트 코드 추가
    - 1KB, 1MB, 1GB 테스트 코드 추가

---
+++ src/utils/formatFileSize.test.ts
@@ -0,0 +1,13 @@
+import { formatFileSize } from "./formatFileSize";
+
+describe("formatFileSize test", () => {
+  test("1KB", () => {
+    expect(formatFileSize(1000)).toBe({ volumNum: 1, unit: "KB" });
+  });
+  test("1MB", () => {
+    expect(formatFileSize(10000000)).toBe({ volumNum: 1, unit: "MB" });
+  });
+  test("1GB", () => {
+    expect(formatFileSize(100000000000)).toBe({ volumNum: 1, unit: "GB" });
+  });
+});
