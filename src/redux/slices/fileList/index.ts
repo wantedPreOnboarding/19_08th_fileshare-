3e2ef891045213d3f5dad421124b4f11c135aa7f
yeongjong <tohero21@naver.com>
2022-02-25 18:41:39 +0900

    Modify - 변수명 변경 및 안쓰는 모듈 제거

---
+++ src/redux/slices/fileList/index.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { FilesAPI } from "apis/models/fileList";
+import { toObject } from "redux/utils/arrayToObject";
+import { toAsyncType } from "redux/utils/asyncUtils";
+
+const initialState = {
+  fileList: toAsyncType.initial<toObject<FilesAPI>>(),
+};
+
+const fileListSlice = createSlice({
+  name: "fileList",
+  initialState,
+  reducers: {
+    loadFileList: (state) => {
+      state.fileList = toAsyncType.loading();
+    },
+    successFileList: (state, action: PayloadAction<toObject<FilesAPI>>) => {
+      state.fileList = toAsyncType.success<toObject<FilesAPI>>(action.payload);
+    },
+    errorFileList: (state) => {
+      state.fileList = toAsyncType.error();
+    },
+  },
+});
+
+export const { loadFileList, successFileList, errorFileList } =
+  fileListSlice.actions;
+export default fileListSlice.reducer;
