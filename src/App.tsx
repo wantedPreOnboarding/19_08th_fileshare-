a2c529e97cd6d94dffe9ed661a97bed58172791a
yeongjong <tohero21@naver.com>
2022-02-25 16:53:02 +0900

    Add - fileList dispatch 및 selector 테스트 임시 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "components/Container";
 import DetailPage from "pages/DetailPage";
 import LinkPage from "pages/LinkPage";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "styles/GlobalStyle";
+import { useAppDispatch, useAppSelector } from "hooks/useStore";
+import { loadFileList } from "redux/slices/fileList";
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadFileList());
+  }, []);
+
+  const data = useAppSelector((state) => state.fileList);
+
   return (
     <>
       <GlobalStyle />
