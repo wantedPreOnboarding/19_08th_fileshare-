2a49c3513158164225e66246c466853343ef9844
Sooim Choi <weniger2021@gmail.com>
2022-02-26 01:35:28 +0900

    Modify formatdate 월 오류 변경

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -3,6 +3,6 @@ const formatDate=(date:number):string=>{
     let newDate= new Date(date*1000);
     const timeZone= newDate.getTimezoneOffset()/60
     const newTime=  0 < timeZone &&timeZone < 10 ? '-0' + Math.abs(timeZone): -10 < timeZone &&timeZone < 0? '+0' + Math.abs(timeZone):10<timeZone?"+"+timeZone:timeZone;
-    return `${newDate.getFullYear()}년 ${tensDigit(newDate.getMonth())+1}월 ${tensDigit(newDate.getDate())}일 ${tensDigit(newDate.getHours())}:${tensDigit(newDate.getMinutes())} ${newTime}:00`}
+    return `${newDate.getFullYear()}년 ${tensDigit(newDate.getMonth()+1)}월 ${tensDigit(newDate.getDate())}일 ${tensDigit(newDate.getHours())}:${tensDigit(newDate.getMinutes())} ${newTime}:00`}
 
 export default formatDate;
