57e73cef7d848a7deb24eb55bb6c65ed942b3e2a
Sooim Choi <weniger2021@gmail.com>
2022-02-25 22:04:14 +0900

    Add 유틸함수 index.ts생성및 파일이름 변경

--- src/utils/printFileSize.ts
+++ src/utils/printFileSize.ts
@@ -1,7 +1,7 @@
 import commaNumber from "./commaNumber";
 import formatFileSize from "./formatFileSize";
 
-export default function printFilesize(bytes: number): string {
+export default function printFileSize(bytes: number): string {
   const formatSize = formatFileSize(bytes);
   const commaSize = commaNumber(formatSize.volumNum);
   return `${commaSize} ${formatSize.unit}`;
