f4ac0fa0a6ca0d3ce41f787caf3a0f5072aeadd8
yeongjong <tohero21@naver.com>
2022-03-07 04:33:12 +0900

    Add - 코어에 응답 받는 형식 등의 config를 입력 받을 수 있도록 설정

--- src/apis/core.ts
+++ src/apis/core.ts
@@ -1,12 +1,12 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 const instance = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-  get: <T>(url: string) =>
-    instance.get<any, AxiosResponse<T>, any>(url).then(responseBody),
+  get: <T>(url: string, config?: AxiosRequestConfig) =>
+    instance.get<any, AxiosResponse<T>, any>(url, config).then(responseBody),
 };
 
 export default requests;
